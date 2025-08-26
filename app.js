bf6ffd416b8493281755882967e7313dcecd5c49
EchoOfHer <6631501037@lamduan.mfu.ac.th>
2025-08-26 13:54:26 +0700

    adding register service for input initial data

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //register password just for input initial user data
-
+app.post('/register', (req, res) => {
+    const {username, password} = req.body;
+    // Hash the password
+    bcrypt.hash(password, 10, function(err, hash) {
+        if(err) {
+            return res.status(500).send('Hashing error');
+        }
+        const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        con.query(sql, [username, hash], function(err, results) {
+            if(err) {
+                return res.status(500).send("Database server error");
+            }
+            res.send("Registration successful!");
+        });
+    });
+});
 //login
 
 //All expense
